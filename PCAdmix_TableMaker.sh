615284e7ccde5ac03e09feb81b381ce9f1c9a335
Enrico Bazzicalupo <enricobazzical@gmail.com>
2018-11-14 16:18:52 +0100

    Progress on TableMaker.sh Adjustments to Admixture script

--- PCAdmix_TableMaker.sh
+++ PCAdmix_TableMaker.sh
@@ -16,9 +16,11 @@ PCAdmix=/home/ebazzicalupo/Admixture/PCAdmix/PCAdmix3_linux # PCAdmix software p
 cd $Admx_PATH/$1
 scaffolds=$(ls | sed 's/PCAdmix_//g')
 
-echo " - Generating table 1 ... - "
+rm $2_Table1.txt
 
-echo -e "SCAFFOLD\tSTART\tEND\tLENGTH\tTOT_WIN\tA_LP_WIN\tB_LP_WIN\tA_PCA_INTRO\tB_PCA_INTRO\tA_WPROP_INTRO\tB_WPROP_INTRO" > Table1.txt
+echo " - Generating Table 1 for $2 in $1... - "
+
+echo -e "SCAFFOLD\tSTART\tEND\tLENGTH\tTOT_WIN\tA_LP_WIN\tB_LP_WIN\tA_PCA_INTRO\tB_PCA_INTRO\tA_WPROP_INTRO\tB_WPROP_INTRO" > $2_Table1.txt
 
 for scaffold in $scaffolds
     do
@@ -26,10 +28,10 @@ for scaffold in $scaffolds
     echo " - ${scaffold} - "
     
     SCAFFOLD=$(echo "${scaffold}")
-    START=$(awk -v FS=" " 'NF>19' PCAdmix_${scaffold}/pcadmix_${scaffold}.markers.txt | cut -f2- | cut -d " " -f1 | head -1 | cut -d ":" -f2 | bc)
-	END=$(awk -v FS=" " 'NF>19' PCAdmix_${scaffold}/pcadmix_${scaffold}.markers.txt | cut -f2- | cut -d " " -f20 | tail -1 | cut -d ":" -f2 | bc)
+    START=$(awk -v FS=" " 'NF>20' PCAdmix_${scaffold}/pcadmix_${scaffold}.markers.txt | cut -f2- | cut -d " " -f1 | head -1 | cut -d ":" -f2 | bc)
+	END=$(awk -v FS=" " 'NF>20' PCAdmix_${scaffold}/pcadmix_${scaffold}.markers.txt | cut -f2- | cut -d " " -f20 | tail -1 | cut -d ":" -f2 | bc)
     LENGTH=$(echo "$END-$START" | bc)
-    TOT_WIN=$(awk -v FS=" " 'NF>19' PCAdmix_${scaffold}/pcadmix_${scaffold}.markers.txt | wc -l)
+    TOT_WIN=$(awk -v FS=" " 'NF>20' PCAdmix_${scaffold}/pcadmix_${scaffold}.markers.txt | wc -l)
     A_LP_WIN=$(rev PCAdmix_${scaffold}/pcadmix_${scaffold}.vit.txt | cut -d " " -f3- | rev | grep $2_A | tr ' ' '\n' | grep -v "$2_A" | grep "1" | wc -l | bc)
     B_LP_WIN=$(rev PCAdmix_${scaffold}/pcadmix_${scaffold}.vit.txt | cut -d " " -f3- | rev | grep $2_B | tr ' ' '\n' | grep -v "$2_B" | grep "1" | wc -l | bc)
     A_PCA_INTRO=$(cat PCAdmix_${scaffold}/pcadmix_${scaffold}.ia.txt | grep $2_A | cut -d " " -f3)
@@ -37,6 +39,34 @@ for scaffold in $scaffolds
     A_WPROP_INTRO=$(echo "scale=4; $A_LP_WIN/$TOT_WIN" | bc | sed 's/^\./0./')
     B_WPROP_INTRO=$(echo "scale=4; $B_LP_WIN/$TOT_WIN" | bc | sed 's/^\./0./')
     
-    echo 
+    echo -e  "$SCAFFOLD\t$START\t$END\t$LENGTH\t$TOT_WIN\t$A_LP_WIN\t$B_LP_WIN\t$A_PCA_INTRO\t$B_PCA_INTRO\t$A_WPROP_INTRO\t$B_WPROP_INTRO" >> $2_Table1.txt
     
 done
+
+echo " - Table 1 for $2 in $1 Generated! -"
+
+## Table 2
+
+echo " - Generating Table 2 for $2 in $1... - "
+
+for scaffold in $scaffolds
+    do
+    
+    echo " - ${scaffold} - "
+	
+	nscaf=$(awk -v FS=" " 'NF>20' PCAdmix_${scaffold}/pcadmix_${scaffold}.markers.txt | cut -f1 | grep -o "Window" | wc -l)
+		
+	paste <(yes "${scaffold}" | head -$nscaf) <(paste -d '.' <(awk -v FS=" " 'NF>20' PCAdmix_${scaffold}/pcadmix_${scaffold}.markers.txt | cut -f1 | grep -o "Window") <(seq -f "%03g" 1 $(awk -v FS=" " 'NF>20' PCAdmix_${scaffold}/pcadmix_${scaffold}.markers.txt | wc -l))) <(rev PCAdmix_${scaffold}/pcadmix_${scaffold}.vit.txt | cut -d " " -f3- | rev | grep $2_A | tr ' ' '\n' | grep -v "$2_A") <(rev PCAdmix_${scaffold}/pcadmix_${scaffold}.vit.txt | cut -d " " -f3- | rev | grep $2_B | tr ' ' '\n' | grep -v "$2_B") <(awk -v FS=" " 'NF>20' PCAdmix_${scaffold}/pcadmix_${scaffold}.markers.txt | cut -f2- | cut -d " " -f1 | cut -d ":" -f2) <(awk -v FS=" " 'NF>20' PCAdmix_${scaffold}/pcadmix_${scaffold}.markers.txt | cut -f2- | cut -d " " -f20 | cut -d ":" -f2) > PCAdmix_${scaffold}/pcadmix_${scaffold}_$2_Table2.txt
+	
+done
+
+echo " - concatenating... -"
+
+cat PCAdmix_*/*_$2_Table2.txt > $Admx_PATH/$1/$2_Table2.txt
+
+echo " - Table 2 for $2 in $1 Generated! -"
+
+
+
+
+
